631daa71939db43277d520c9edf36f94d9dfee4c
Maxwell Yang <gabrielyang233@outlook.com>
2022-05-03 23:29:14 -0400

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+  theme: {
+    extend: {
+			colors: {
+				'bgcolor': '#f1faee',
+				'highlight': '#e63946',
+				'text': '#1d3557'
+			},
+		},
+  },
+  plugins: [],
+}
+
