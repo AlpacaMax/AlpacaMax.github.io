fdb0084464d14cfe02775f2c2a75ccb5b5c558cc
Maxwell Yang <gabrielyang233@outlook.com>
2022-05-19 21:49:51 -0400

    Initial out subtree commit gh-pages

---
+++ out/_next/static/4gL5j9mZbrOyVuR3WzGmQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-f4af2532c81c9a51.js","static/chunks/275-1e58899ff98e62af.js","static/chunks/pages/index-12a68af9e6ff88e8.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
