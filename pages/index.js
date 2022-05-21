374478bfd037db6cf50ba4a5b716ae7951b71b92
Maxwell Yang <gabrielyang233@outlook.com>
2022-05-20 22:52:34 -0400

    Adjust index page layout

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { FaGithub, FaTwitter } from 'react-icons/fa';
 export default function Home() {
   return (
 		<div>
-			<div className="container mx-auto flex flex-col">
+			<div className="h-screen container mx-auto flex flex-col">
 				<div className="flex flex-row">
 					<div className="grow flex flex-row">
 						<div className="bg-highlight py-8 px-6 rounded-b-lg">
@@ -29,7 +29,7 @@ export default function Home() {
 					</div>
 				</div>
 
-				<div className="grow my-28 text-text">
+				<div className="grow text-text flex flex-col justify-center">
 					<p className="text-2xl font-light">
 						Hello, I&apos;m
 					</p>
@@ -39,29 +39,30 @@ export default function Home() {
 					<p className="font-light text-xl">
 						Student, Reader, Software Developer, Open Source Contributor, Dreamer 
 					</p>
-					<p className="text-xl mt-14">
+					<p className="text-xl mt-20">
 						I’m a student majoring Computer Science at New York University Tandon School of Engineering. I’ve been a relentless programmer since I was 13. In my free time I read books, watch TV series and anime, and code. Occasionally I play chess and tetris and draw pixel art.
 					</p>
 					<p className="text-xl mt-4">
 	I mostly code in Python. Recently I’ve also been taking a deeper dive into C. Additionally, I code in Javascript, Lua, Bash and sometimes Go. In total I’ve used 15 programming languages already and I’ll try even more in the future!
 					</p>
+
+					<div className="mt-20">
+						<a 
+							className="bg-highlight text-bgcolor text-lg py-3 px-6 rounded-lg"
+							href="https://docs.google.com/document/d/e/2PACX-1vRrAs7amMwkWyspFxdKKoX0BsiYwYxhU3AUe-OWMQMFW2t095QKBol3bOaN03ZEo3hnIPUMibWWvSp0/pub"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Read My CV
+						</a>
+					</div>
 				</div>
 
-				<div>
-					<a 
-						className="bg-highlight text-bgcolor text-lg py-3 px-6 rounded-lg"
-						href="https://docs.google.com/document/d/e/2PACX-1vRrAs7amMwkWyspFxdKKoX0BsiYwYxhU3AUe-OWMQMFW2t095QKBol3bOaN03ZEo3hnIPUMibWWvSp0/pub"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Read My CV
-					</a>
-				</div>
+				<footer className="flex flex-row justify-center text-2xl text-text">
+					<a href="https://github.com/AlpacaMax" className="mx-4 my-6" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+					<a href="https://twitter.com/AlpacaMax2000" className="mx-4 my-6" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+				</footer>
 			</div>
-			<footer className="absolute inset-x-0 bottom-0 flex flex-row justify-center text-2xl text-text">
-				<a href="https://github.com/AlpacaMax" className="mx-4 my-6" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-				<a href="https://twitter.com/AlpacaMax2000" className="mx-4 my-6" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-			</footer>
 		</div>
   )
 }
