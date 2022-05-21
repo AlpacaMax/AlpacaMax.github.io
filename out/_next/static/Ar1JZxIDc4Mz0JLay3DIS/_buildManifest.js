89c65b9b0b3acf6edccdfdb446c34b3f48ce43eb
Maxwell Yang <gabrielyang233@outlook.com>
2022-05-20 23:39:46 -0400

    Deploy gh-pages

---
+++ out/_next/static/Ar1JZxIDc4Mz0JLay3DIS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-f4af2532c81c9a51.js","static/chunks/pages/index-a04f8f7f8cfd4b5b.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
