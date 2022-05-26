874316f37ab89c7da0e0dc0f316673fe81e24c68
Maxwell Yang <gabrielyang233@outlook.com>
2022-05-25 20:13:43 -0400

    Add links to my projects

--- pages/projects.js
+++ pages/projects.js
@@ -12,37 +12,73 @@ export default function Projects() {
 						<span>A 1 vs 1 open debate platform implemented in Python + FastAPI + PostgreSQL</span>
 					</div>
 					<div className="justify-self-end font-bold">
-						Mayat
+						<a
+							href="https://github.com/AnubisLMS/Mayat"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Mayat
+						</a>
 					</div>
 					<div className="col-span-2">
 						An AST-based source code plagiarism detection system
 					</div>
 					<div className="justify-self-end font-bold">
-						Anubis
+						<a
+							href="https://github.com/AnubisLMS/Anubis"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Anubis
+						</a>
 					</div>
 					<div className="col-span-2">
 						A Distributed LMS for automating Computer Science courses from NYU
 					</div>
 					<div className="justify-self-end font-bold">
-						Sheep Herding Simulator
+						<a
+							href="https://github.com/AlpacaMax/Sheep_Herding_Simulator"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Sheep Herding Simulator
+						</a>
 					</div>
 					<div className="col-span-2">
 						A 2021 GiGA Game Jam entry implemented in Lua + Love2d
 					</div>
 					<div className="justify-self-end font-bold">
-						Plane Ticket Booking System
+						<a
+							href="https://github.com/AlpacaMax/Plane_Ticket_Booking_System"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Plane Ticket Booking System
+						</a>
 					</div>
 					<div className="col-span-2">
 						A plane ticket booking system implemented in Python + Flask + SQLalchemy
 					</div>
 					<div className="justify-self-end font-bold">
-						XAM
+						<a
+							href="https://alpacamax.itch.io/xam"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							XAM
+						</a>
 					</div>
 					<div className="col-span-2">
 						A WeeklyGameJam entry implemented in Unity3D
 					</div>
 					<div className="justify-self-end font-bold">
-						CHIP8 Emulator
+						<a
+							href="https://github.com/AlpacaMax/Python-CHIP8-Emulator"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							CHIP8 Emulator
+						</a>
 					</div>
 					<div className="col-span-2">
 						A Chip-8 Emulator implemented in Python
