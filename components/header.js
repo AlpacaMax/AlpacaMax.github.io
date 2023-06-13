a82158111a22601bb6e0ecb08556b681e5dadb30
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-06-14 01:14:31 +0800

    Change icon and title

--- components/header.js
+++ components/header.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Header() {
     return (
-		<div className="flex flex-row">
+		<div>
+			<Head>
+				<title>Tian Yang</title>
+			</Head>
 			<div className="flex flex-row">
-				<div className="bg-highlight my-6 py-3 px-6 shadow-[10px_10px_rgba(0,0,0,1)]">
-					<Link href="/" className="text-xl text-black font-light">
-                        TIAN YANG
-					</Link>
+				<div className="flex flex-row">
+					<div className="bg-highlight my-6 py-3 px-6 shadow-[10px_10px_rgba(0,0,0,1)]">
+						<Link href="/" className="text-xl text-black font-light">
+							TIAN YANG
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
