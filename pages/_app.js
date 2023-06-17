c6172b1d9955ff6bc67abc04135196eafdf7a71b
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-06-17 20:42:05 +0800

    Deploy gh-pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component className="bg-bgcolor" {...pageProps} />
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-3FDFB7XF4B"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3FDFB7XF4B', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+      <Component className="bg-bgcolor" {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
